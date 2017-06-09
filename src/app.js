d4fcd65b0ef427dacbdb7b863e23ef56d0c0b0d9
chengpeng <chengpengcp9@163.com>
2017-06-09 17:57:59 +0800

    add config

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import Koa from 'koa'
+import config from './config/config'
+import log4js from 'log4js'
+
 const app = new Koa()
+const LOG = log4js.getLogger('file')
 
 // response
 app.use(async (ctx) => {
     ctx.body = 'Hello World'
 })
 
-app.listen(3000, () => console.log('server started 3000'))
+app.listen(config.app.port)
+LOG.info(`server started, listening on port ${config.app.port}`)
 
 export default app
---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+const apiPort = process.env.NODE_PORT || 3000
+
+module.exports = {
+    app: {
+        port: process.env.NODE_PORT || 3000
+    }
+}
