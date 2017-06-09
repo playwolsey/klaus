18cd821cd1ccee247819d49bb5db38ad40ed6334
chengpeng <chengpengcp9@163.com>
2017-06-09 18:49:03 +0800

    swig view routes added

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 import Koa from 'koa'
 import config from './config/config'
+import routes from './routes'
 import log4js from 'log4js'
 
 const app = new Koa()
 const LOG = log4js.getLogger('file')
 
-// response
-app.use(async (ctx) => {
-    ctx.body = 'Hello World'
-})
+app.use(routes())
+app.use(views(__dirname + '/../views', {extension: 'swig'}))
 
 app.listen(config.app.port)
 LOG.info(`server started, listening on port ${config.app.port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import Router from 'koa-router'
+import compose from 'koa-compose'
+
+const router = new Router()
+
+router.get('/', async(ctx, next) => {
+    await ctx.render('./index')
+})
+
+router.get('*', async (ctx, next) => {
+    ctx.body = { status : 404 }
+})
+
+export default routes => (
+    compose([
+        router.routes(),
+        router.allowedMethods()
+    ])
+) 
