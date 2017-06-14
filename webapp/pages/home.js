236ec5d328b9e99de8cfb98d4039c012c7988bcf
chengpeng <chengpengcp9@163.com>
2017-06-14 14:18:47 +0800

    routes && store

---
+++ webapp/pages/home.js
@@ -0,0 +1,11 @@
+import React , { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>index</div>
+        )
+    }
+} 
+
+export default Home
