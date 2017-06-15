dd23625a6a27589c182648c8e818e7e3cc92a11e
chengpeng <chengpengcp9@163.com>
2017-06-15 17:39:50 +0800

    init components

---
+++ webapp/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+    render() {
+        return (
+            <header id="header" className="m-header">
+            </header>
+        )
+    }
+}
+
+export default Header
