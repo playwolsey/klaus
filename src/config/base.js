19c2f5eac7a960317539de60415b583a5a66bee7
chengpeng <chengpengcp9@163.com>
2017-06-15 17:39:25 +0800

    history api fallback configured

--- src/config/base.js
+++ src/config/base.js
@@ -2,6 +2,8 @@ import serve from 'koa-static'
 import logger from 'koa-logger'
 import mount from 'koa-mount'
 import views from 'koa-views'
+import convert from 'koa-convert'
+import historyApiFallback from 'koa-connect-history-api-fallback'
 import log4js from 'log4js'
 
 export default (app) => {
@@ -10,4 +12,8 @@ export default (app) => {
     app.use(logger())
     app.use(mount("/", serve(`${__dirname}/../public/`)))
     app.use(views(`${__dirname}/../views`, {extension: 'swig'}))
+
+    app.use(convert(historyApiFallback({
+        index: '/'
+    })))
 }
