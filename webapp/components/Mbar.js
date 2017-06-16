65997453b1ccf58fd7b26ea3800e342ddc9ef334
chengpeng <chengpengcp9@163.com>
2017-06-16 17:21:08 +0800

    Mbar init

---
+++ webapp/components/Mbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Mbar extends Component {
+    render() {
+        return (
+            <section id="mbar" className="m-mbar">
+            </section>
+        )
+    }
+}
+
+export default Mbar
