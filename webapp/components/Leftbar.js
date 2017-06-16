09a547be4983a9bcbcf69a790d3a2e7510f072ba
chengpeng <chengpengcp9@163.com>
2017-06-16 10:33:37 +0800

    init leftbar

---
+++ webapp/components/Leftbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Leftbar extends Component {
+    render() {
+        return (
+            <section id="leftbar" className="m-leftbar">
+            </section>
+        )
+    }
+}
+
+export default Leftbar
