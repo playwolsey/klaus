4d6d3e6ffc75ef370cc1ec3a6949c7909698ebc6
chengpeng <chengpengcp9@163.com>
2017-06-16 17:22:30 +0800

    init st

--- webapp/components/Header.js
+++ webapp/components/Header.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 
 class Header extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            isToggleOn: true
+        }
+
+        this.toggleBar = () => {
+            this.setState(prevState => ({
+                isToggleOn: !prevState.isToggleOn
+            }))
+        }
+    }
+
     render() {
         return (
-            <header id="header" className="m-header">
+            <header id='header' className='m-header'>
+                <a className={`m-toggle ${this.state.isToggleOn ? 'on' : 'off'}`} href='javascript:void(0)' onClick={this.toggleBar}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </a>
             </header>
         )
     }
