e9957ad3e8d67bfc1eaefd152b36e5b8ad28b0d8
chengpeng <chengpengcp9@163.com>
2017-06-22 17:55:52 +0800

    rm comments

--- webapp/store/store.js
+++ webapp/store/store.js
@@ -1,17 +1,10 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
-//import * as reducer from '../reducers/index'
 import rootReducer from '../reducers/root'
 import thunk from 'redux-thunk'
-import { createLogger } from 'redux-thunk'
 
 const middlewares = [thunk]
 
-if (process.env.NODE_ENV === `development`) {
-    //middlewares.push(createLogger())
-}
-
 var store = createStore(
-    //combineReducers(reducer),
     rootReducer,
     applyMiddleware(...middlewares)
 );
