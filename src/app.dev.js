75bd83d2a15c67aa26407f50a7203160efd3b1c9
chengpeng <chengpengcp9@163.com>
2017-06-22 16:24:12 +0800

    rename webpack config to dev config

--- src/app.dev.js
+++ src/app.dev.js
@@ -1,16 +1,13 @@
 import Koa from 'koa'
 import views from 'koa-views'
-//import convert from 'koa-convert'
 import base from './config/base'
 import config from './config/config'
 import routes from './routes'
 import log4js from 'log4js'
 
 import webpack from 'webpack'
-//import webpackDevMiddleware from 'koa-webpack-dev-middleware'
-//import webpackHotMiddleware from 'koa-webpack-hot-middleware'
 import { devMiddleware, hotMiddleware } from 'koa-webpack-middleware'
-import webpackConfig from '../webpack.config'
+import webpackConfig from '../webpack.dev.config'
 
 const app = new Koa()
 const LOG = log4js.getLogger('file')
