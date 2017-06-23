3f512bc4b42bf4d918a445960df4f51c4e2511c6
chengpeng <chengpengcp9@163.com>
2017-06-23 17:37:19 +0800

    init actions

---
+++ webapp/actions/index.js
@@ -0,0 +1,19 @@
+//export GET_ALL_PROJECTS = 'GET_ALL_PROJECTS'
+import 'whatwg-fetch'
+
+export const getAllProjects = () => {
+    return dispatch => {
+        fetch('/projects/all', {
+            "Content-Type": "application/json"
+        })
+        .then(response => {
+            return response.json()
+        })
+        .then(tjson => {
+            dispatch({
+                type: 'GET_ALL_PROJECTS',
+                data: tjson.data
+            })
+        })
+    }
+} 
