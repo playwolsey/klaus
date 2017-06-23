9028a2c994139347034fadb5c89477c6d79645a2
chengpeng <chengpengcp9@163.com>
2017-06-23 13:32:30 +0800

    add logger to story

--- webapp/store/store.js
+++ webapp/store/store.js
@@ -1,8 +1,13 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import rootReducer from '../reducers/root'
 import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 
 const middlewares = [thunk]
+
+if (process.env.NODE_ENV === `development`) {
+    middlewares.push(createLogger())
+}
 
 var store = createStore(
     rootReducer,
