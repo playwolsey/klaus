cfd5046c01223092345bebcc6f306d1abfc52e97
chengpeng <chengpengcp9@163.com>
2017-06-29 09:45:58 +0800

    add connect to actions

--- webapp/components/Leftbar.js
+++ webapp/components/Leftbar.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { getAllProjects } from '../actions/index'
 
 class Leftbar extends Component {
+    //constructor(props) {
+    //    super(props)
+    //    //this.handleChange = this.handleChange.bind(this)
+    //    //this.handleRefreshClick = this.handleRefreshClick.bind(this)
+    //}
+
+    componentDidMount() {
+        const { dispatch } = this.props
+        dispatch(getAllProjects())
+    }
+
     render() {
         return (
             <section id="leftbar" className="m-leftbar">
@@ -9,4 +23,15 @@ class Leftbar extends Component {
     }
 }
 
-export default Leftbar
+Leftbar.propTypes = {
+    dispatch: PropTypes.func.isRequired
+}
+
+function mapStateToProps(state) {
+    const { projects } = state
+    return {
+        projects
+    }
+}
+
+export default connect(mapStateToProps)(Leftbar)
