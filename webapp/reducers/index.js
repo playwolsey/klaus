c0b0138933dc510fb7b9d48524a165d980153ca2
chengpeng <chengpengcp9@163.com>
2017-06-29 09:48:40 +0800

    init state & constants fi

--- webapp/reducers/index.js
+++ webapp/reducers/index.js
@@ -1,4 +1,20 @@
-const index = (state = {}, action = {}) => {
+import * as types from '../constants/ActionTypes'
+
+const initialState = {
+    projects: []
+}
+
+const index = (state = initialState, action) => {
+    switch(action.type) {
+        case types.GET_ALL_PROJECTS: {
+            return Object.assign({}, state, {
+                projects: action.data.projects
+            })
+        }
+
+        default: 
+            return state
+    }
     return state
 }
 
