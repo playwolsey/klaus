b03910bd61b22f987be625f3fc4689501e883f02
chengpeng <chengpengcp9@163.com>
2017-07-04 09:53:38 +0800

    add List component

--- webapp/components/Leftbar.js
+++ webapp/components/Leftbar.js
@@ -3,15 +3,39 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { getAllProjects } from '../actions/index'
 
+//class ListItem extends Component {
+//    render() {
+//        return (
+//            <li className='project_li'>
+//            </li>
+//        )
+//    }
+//}
+
+class List extends Component {
+    render() {
+        return (
+            <ul className='m-projects-list'>
+            {
+                this.props.projects.map((item, index) => {
+                    return <li className='projects-li' key={index}>{item.name}</li>
+                })
+            }
+            </ul>
+        )
+    }
+}
+
 class Leftbar extends Component {
-    //constructor(props) {
-    //    super(props)
-    //    //this.handleChange = this.handleChange.bind(this)
-    //    //this.handleRefreshClick = this.handleRefreshClick.bind(this)
-    //}
+    constructor(props) {
+        super(props)
+        //this.handleChange = this.handleChange.bind(this)
+        //this.handleRefreshClick = this.handleRefreshClick.bind(this)
+    }
 
     componentDidMount() {
         const { dispatch } = this.props
+        console.log(this.props)
         dispatch(getAllProjects())
     }
 
