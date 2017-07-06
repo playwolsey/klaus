1b0783a86683831202d98b1795f2f1e0dd220eee
chengpeng <chengpengcp9@163.com>
2017-07-06 11:17:33 +0800

    change string to constants

--- webapp/actions/index.js
+++ webapp/actions/index.js
@@ -1,16 +1,17 @@
 import 'whatwg-fetch'
+import * as types from '../constants/ActionTypes'
 
 export const getAllProjects = () => {
     return dispatch => {
         fetch('/projects/all', {
-            "Content-Type": "application/json"
+            credentials: 'same-origin'
         })
         .then(response => {
             return response.json()
         })
         .then(tjson => {
             dispatch({
-                type: 'GET_ALL_PROJECTS',
+                type: types.GET_ALL_PROJECTS,
                 data: tjson.data
             })
         })
