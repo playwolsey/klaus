baf002603143cbba0d5d93852d70cbc132f8baba
chengpeng <chengpengcp9@163.com>
2017-07-07 16:21:14 +0800

    rename api detail component

---
+++ webapp/components/ApiDetail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ApiContainer extends Component {
+    render() {
+        return (
+            <section className='m-container'>
+            </section>
+        )
+    }
+}
+
+export default ApiContainer
