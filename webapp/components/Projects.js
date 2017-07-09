03cf2817679a767d42e2cbc6fa9e45b3a1433c7c
chengpeng <chengpengcp9@163.com>
2017-07-09 20:55:44 +0800

    pro style

--- webapp/components/Projects.js
+++ webapp/components/Projects.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+import '../style/projects.scss'
+
 export default class Projects extends Component {
     static propTypes = {
         projects: PropTypes.array.isRequired
