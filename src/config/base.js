a15f66b1f61779e3bf4d64eba8515503282dd297
chengpeng <chengpengcp9@163.com>
2017-07-26 09:39:45 +0800

    koa body init

--- src/config/base.js
+++ src/config/base.js
@@ -3,6 +3,7 @@ import logger from 'koa-logger'
 import mount from 'koa-mount'
 import views from 'koa-views'
 import convert from 'koa-convert'
+import koaBody from 'koa-body'
 import historyApiFallback from 'koa-connect-history-api-fallback'
 import log4js from 'log4js'
 
@@ -10,6 +11,7 @@ export default (app) => {
     app.proxy = true
 
     app.use(logger())
+    app.use(koaBody())
     app.use(mount("/", serve(`${__dirname}/../../public/`)))
     app.use(views(`${__dirname}/../views`, {extension: 'swig'}))
 
