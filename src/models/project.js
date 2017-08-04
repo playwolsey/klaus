07ad7c7bffea41be19106613aa589e7ea679c776
chengpeng <chengpengcp9@163.com>
2017-08-04 10:43:15 +0800

    fix bug for insert table

--- src/models/project.js
+++ src/models/project.js
@@ -2,17 +2,12 @@ import sequelize from './sequelize'
 import Sequelize from 'sequelize' 
 
 const Project = sequelize.define('project', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
     name: {
         type: Sequelize.STRING,
         allowNull: false
     }
 })
 
-//Project.sync()
+// Project.sync()
 
 export default Project
